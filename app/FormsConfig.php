6786d8aa513efbe0cd56f6679b3f9d22de568d73
Rahul sharma <rahuls.techmarbles@gmail.com>
2020-03-05 16:21:16 +0000

    audit modal added

--- app/FormsConfig.php
+++ app/FormsConfig.php
@@ -59,11 +59,9 @@ class FormsConfig extends Model
     	return self::where(['tab' => $tab, 'grp' => $group])->get();
     }
 
-
-    public static function GetTab( $tabname ){  
-    	
+    public static function GetTab( $tabname )
+    {
     	return self::where(['tab' => $tabname, 'is_active' => 'yes'])->get(); 
-
 	} 
 
 }
