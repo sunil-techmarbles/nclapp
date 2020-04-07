0425b826972af2c15bdfe15ea6e48be06b135b52
Rahul sharma <rahul.techmarbles@gmail.com>
2020-04-07 11:33:59 +0530

    import section issue

--- resources/views/admin/import/list.blade.php
+++ resources/views/admin/import/list.blade.php
@@ -4,7 +4,7 @@
 <div class="container shopify-product-table">
     <div id="page-head" class="noprint">Import</div>
     <form action="{{route('import')}}" method="GET" class="search-table" enctype="multipart/form-data">
-        @if (request()->get('model') && empty($shopify_priceData))
+        @if (request()->get('model') && empty($shopifyPriceData))
             <p class="alert alert-danger"> No Data Found.</p>
         @endif
         <div class="row" style="margin-bottom:5px">
@@ -38,47 +38,69 @@
             </div> 
         </div>
         @if(!empty($shopifyPriceData))
-            <h2>Final Price : $<span class="show_final_price">{{$shopifyPriceData[0]['Final_Price']}}</span></h2>
-            <input class="show_all" type="button" value="Show All Records">
+            <div class="row mt-2">
+                <div class="col-md-9">
+                    <h2>Final Price : $<span class="show_final_price">{{$shopifyPriceData[0]['Final_Price']}}</span></h2>
+                </div>
+                <div class="col-md-3">
+                    <input class="show_all  float-right btn btn-default border border-primary" type="button" value="Show All Records">
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col-md-4">
+                    <select name="properties[Hard Drive]" class="hard_drive form-control">
+                        <option value="0">-- Choose Hard Drive --</option>
+                        <option value="0" data-option_value_key="0">320GB SATA</option>
+                        <option value="20" data-option_value_key="1">500GB SATA [+$20.00]</option>
+                        <option value="25" data-option_value_key="2">1TB SATA [+$25.00]</option>
+                        <option value="30" data-option_value_key="3">128GB SSD [+$30.00]</option>
+                        <option value="35" data-option_value_key="4">256GB SSD [+$35.00]</option>
+                        <option value="50" data-option_value_key="5">512GB SSD [+$50.00]</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <select name="properties[Memory]" class="memory form-control">
+                        <option value="0">-- Choose Memory --</option>
+                        <option value="0" data-option_value_key="0">4GB</option>
+                        <option value="20" data-option_value_key="1">8GB [+$20.00]</option>
+                        <option value="40" data-option_value_key="2">16GB [+$40.00]</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                     <select name="properties[Operating System]" class="operating_system form-control">
+                        <option value="0">-- Choose Operating System --</option>
+                        <option value="0" data-option_value_key="0">No Operating System Needed</option>
+                        <option value="19.19" data-option_value_key="1">Windows 10 Home 64-Bit [+$19.99]</option>
+                        <option value="39.99" data-option_value_key="2">Windows 10 Pro 64-Bit [+$39.99]</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col-md-4">
+                    <select name="properties[Software]" class="software form-control">
+                        <option value="0">-- Choose Software --</option>
+                        <option value="0" data-option_value_key="0">No Additional Software</option>
+                        <option value="49.99" data-option_value_key="1">Microsoft Office 365 - 2019 [+$49.99]</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <select name="properties[Warranty]" class="warranty form-control">
+                        <option value="0">-- Choose Warranty --</option>
+                        <option value="0" data-option_value_key="0">90 Day Standard Warranty</option>
+                        <option value="29.99" data-option_value_key="1">One Year - Support &amp; Maintenance [+$29.99]</option>
+                        <option value="49.99" data-option_value_key="2">Two Year - Support &amp; Maintenance [+$49.99]</option>
+                        <option value="74.99" data-option_value_key="3">Three Year - Support &amp; Maintenance [+$74.99]</option>
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <select name="properties[Accessories]" class="accessories form-control">
+                        <option value="0">-- Choose Accessories --</option>
+                        <option value="0" data-option_value_key="0">No Accessories Needed</option>
+                        <option value="199.99" data-option_value_key="1">Refurbished HP Laserjet Desktop Printer [+$199.99]</option>
+                    </select>
+                </div>
+            </div>
             <input type="hidden" class="final_price" value="{{$shopifyPriceData[0]['Final_Price']}}">
-            <select name="properties[Hard Drive]" class="hard_drive">
-                <option value="0">-- Choose Hard Drive --</option>
-                <option value="0" data-option_value_key="0">320GB SATA</option>
-                <option value="20" data-option_value_key="1">500GB SATA [+$20.00]</option>
-                <option value="25" data-option_value_key="2">1TB SATA [+$25.00]</option>
-                <option value="30" data-option_value_key="3">128GB SSD [+$30.00]</option>
-                <option value="35" data-option_value_key="4">256GB SSD [+$35.00]</option>
-                <option value="50" data-option_value_key="5">512GB SSD [+$50.00]</option>
-            </select>
-            <select name="properties[Memory]" class="memory">
-                <option value="0">-- Choose Memory --</option>
-                <option value="0" data-option_value_key="0">4GB</option>
-                <option value="20" data-option_value_key="1">8GB [+$20.00]</option>
-                <option value="40" data-option_value_key="2">16GB [+$40.00]</option>
-            </select>
-            <select name="properties[Operating System]" class="operating_system">
-                <option value="0">-- Choose Operating System --</option>
-                <option value="0" data-option_value_key="0">No Operating System Needed</option>
-                <option value="19.19" data-option_value_key="1">Windows 10 Home 64-Bit [+$19.99]</option>
-                <option value="39.99" data-option_value_key="2">Windows 10 Pro 64-Bit [+$39.99]</option>
-            </select>
-            <select name="properties[Software]" class="software">
-                <option value="0">-- Choose Software --</option>
-                <option value="0" data-option_value_key="0">No Additional Software</option>
-                <option value="49.99" data-option_value_key="1">Microsoft Office 365 - 2019 [+$49.99]</option>
-            </select>
-            <select name="properties[Warranty]" class="warranty">
-                <option value="0">-- Choose Warranty --</option>
-                <option value="0" data-option_value_key="0">90 Day Standard Warranty</option>
-                <option value="29.99" data-option_value_key="1">One Year - Support &amp; Maintenance [+$29.99]</option>
-                <option value="49.99" data-option_value_key="2">Two Year - Support &amp; Maintenance [+$49.99]</option>
-                <option value="74.99" data-option_value_key="3">Three Year - Support &amp; Maintenance [+$74.99]</option>
-            </select>
-            <select name="properties[Accessories]" class="accessories">
-                <option value="0">-- Choose Accessories --</option>
-                <option value="0" data-option_value_key="0">No Accessories Needed</option>
-                <option value="199.99" data-option_value_key="1">Refurbished HP Laserjet Desktop Printer [+$199.99]</option>
-            </select>
        @endif
     </form>
     @if (!empty($shopifyPriceData))
