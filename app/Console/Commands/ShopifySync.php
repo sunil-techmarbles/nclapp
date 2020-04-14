ef5653dfef9f9b79ed6b08c0623d7bb011de2023
Manish GAutam <manish.work.techmarbles@gmail.com>
2020-04-14 08:14:09 +0000

    shopitf sync cron

--- app/Console/Commands/ShopifySync.php
+++ app/Console/Commands/ShopifySync.php
@@ -6,6 +6,8 @@ use Illuminate\Console\Command;
 
 class ShopifySync extends Command
 {
+    public $skuToIgnore = ['HP-600-M602-CE390A','HP-400-M401dn-CF280X','HP-P2035-CE505A','HP-400-M401dn-CF280X','HP-P2055dn-05A-CE505A','HP-600-M601-CE390A','HP-400-m401' ];
+    
     /**
      * The name and signature of the console command.
      *
@@ -37,6 +39,8 @@ class ShopifySync extends Command
      */
     public function handle()
     {
+
+
         die("sync shopify");
     }
 }
